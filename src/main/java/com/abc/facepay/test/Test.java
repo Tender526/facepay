d38b1eb1c86819b8fb3ff49c9e85656b6ce93175
who <who123@gmail.com>
2023-03-05 13:50:46 +0800

    3 commit

--- src/main/java/com/abc/facepay/test/Test.java
+++ src/main/java/com/abc/facepay/test/Test.java
@@ -3,5 +3,6 @@ package com.abc.facepay.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("ceshi commit");
+        System.out.println("ceshi commit2");
     }
 }
